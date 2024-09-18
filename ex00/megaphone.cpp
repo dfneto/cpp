9c4d4bba6e96f96d927ed99eff3579e28bcf8703
dfneto <pdavidfneto@gmail.com>
2024-09-18 17:06:20 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+    
+    if (argc == 1)
+	{
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    	return (0);
+	}
+    int i = 0;
+	argv++;
+    while (*argv)
+    {
+		while ((*argv)[i])
+        {
+			std::cout << (char)std::toupper((*argv)[i]);
+			i++;
+		}
+		argv++;
+    }
+	std::cout << std::endl;
+	return (0);
+}
