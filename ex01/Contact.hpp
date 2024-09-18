853cdcb5da55799b41aa4708aa67deab5cb10d43
dfneto <pdavidfneto@gmail.com>
2024-09-18 17:55:24 +0200

    ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,19 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string nickName;
+        std::string phone;
+        std::string darkSecret;
+    public:
+        
+
+};
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "PhoneBook.hpp"
+
+int main(int argc, char **argv) {
+
+    std::string     userInput;
+    PhoneBook       phoneBook;
+
+    std::cout << "Please enter some input: ";
+    std::getline(std::cin, userInput);
+
+    while (userInput != "EXIT")
+    {
+
+    }
+    return (0);
+}
