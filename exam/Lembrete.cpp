a660ffcb0e698d7a72866ffa05643b34ea4de2a7
dfneto <pdavidfneto@gmail.com>
2024-12-12 13:36:55 +0100

    exam - parei no 1

---
+++ exam/Lembrete.cpp
@@ -0,0 +1,18 @@
+
+
+
+
+Constructor: ClassName::ClassName(Args);
+
+ClassName &ClassName::operator=(const ClassName &ref) {}
+
+<return_type> ClassName::<function_name>(<parameters>) [qualifiers] [noexcept];
+void Warlock::introduce() const {}
+const std::string& Warlock::getName() const {}
+
+
+
+pure method: virtual e =0
+Also add a clone pure method that returns a pointer to ASpell:
+virtual ASpell* clone() const = 0;
+
